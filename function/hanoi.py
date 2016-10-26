3accb1d26dc8410be7d9131d5f93ea10ed6c8ed5
esdream <conquerfu@gmail.com>
2016-10-26 13:45:26 +0800

    learn function

---
+++ function/hanoi.py
@@ -0,0 +1,9 @@
+def hanoi(n, a, b, c):
+    if n == 1:
+        print(a, '-->', c)
+    else:
+        hanoi(n-1, a, c, b)
+        hanoi(1, a, b, c)
+        hanoi(n-1, b, a, c)
+
+hanoi(5, 'A', 'B', 'C')
