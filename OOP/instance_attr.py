98e1fcf139b9319a351d6d45d022e252ad7b2f8c
esdream <conquerfu@gmail.com>
2016-12-08 21:10:24 +0800

    oop WIP

---
+++ OOP/instance_attr.py
@@ -0,0 +1,7 @@
+class Student():
+    pass
+s = Student()
+s.name = 'Can'
+
+print(dir(s))
+print(dir(Student))
---
+++ test.py
@@ -0,0 +1,9 @@
+class Test(object):
+    pass
+
+t1 = Test()
+t2 = Test()
+print(t1)
+print(t2)
+
+print(type(t1))
