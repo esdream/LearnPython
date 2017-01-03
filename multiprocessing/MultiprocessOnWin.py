5954bd8648e8b344ae50017d78f362e1bd8632bd
esdream <conquerfu@gmail.com>
2017-01-03 21:34:41 +0800

    process_WIP2

--- multiprocessing/MultiprocessOnWin.py
+++ multiprocessing/MultiprocessOnWin.py
@@ -1,11 +1,12 @@
 from multiprocessing import Process
 import os
 
-def run_proc(name):
-    print('Run child process %s (%s)...' % (name, os.getpid()))
+def run_proc(*args):
+    var = args
+    print('Run child process %s (%s)...' % (var[0], os.getpid()))
 if __name__ == '__main__':
     print('Parent process %s.' % os.getpid())
-    p = Process(target=run_proc, args=('test',))
+    p = Process(target=run_proc, args=('test','num'))
     print('Child process will start')
     p.start()
     p.join()
