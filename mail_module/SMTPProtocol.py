cea4f6e31aa22ba67387adc1fc09a97c6033d3e0
esdream <conquerfu@gmail.com>
2017-05-08 15:45:45 +0800

    'SMTP_finished'

--- mail_module/SMTPProtocol.py
+++ mail_module/SMTPProtocol.py
@@ -13,26 +13,25 @@ def _format_addr(s):
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
 # 输入Email地址和口令
-from_addr = 'badixue@163.com'
-password = 'fuMG1627049'
+from_addr = '' # 发送方Email地址
+password = ''   # 163,qq等邮箱提供口令进行SMTP发送，与密码区别开
 # 输入收件人地址
-to_addr = '344879874@qq.com'
+to_addr = ''    # 收件方Email地址
 # 输入SMTP服务器地址
 smtp_server = 'smtp.163.com'
 
-
 # 邮件对象
-msg = MIMEMultipart()
+msg = MIMEMultipart('alternative')
 msg['From'] = _format_addr('Python Learner <%s>' % from_addr)
 msg['To'] = _format_addr('Administrator <%s>' % to_addr)
 msg['Subject'] = Header('From SMTP call...', 'utf-8').encode()
 
 # 邮件正文
-msg.attach(MIMEText('<h1>hello</h1>, send by <p><a href="http://www.python.org">Python...</a></p>', 'html', 'utf-8'))
+msg.attach(MIMEText('hello', 'plain', 'utf-8'))
+msg.attach(MIMEText('<h1>hello</h1>, send by <p><a href="http://www.python.org">Python...</a><br><img src="cid:0"></p>', 'html', 'utf-8'))
 
 # 添加附件
 file_dir = os.path.join(os.path.abspath('.'), 'test.jpg')
-print(file_dir)
 with open(file_dir, 'rb') as f:
     # 设置附件的MIME和文件名
     mime = MIMEBase('image', 'jpg', filename = 'test.jpg')
@@ -47,8 +46,9 @@ with open(file_dir, 'rb') as f:
     # 添加到MIMEMultipart
     msg.attach(mime)
 
-
 server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
+# server.starttls()
+
 server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
